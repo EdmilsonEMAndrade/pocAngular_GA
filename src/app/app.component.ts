bdf8ad756748def23eeae43dec07b76748672f3f
Thiago Moraes <thiago.luiz@sulamerica.com.br>
2022-05-04 11:20:20 -0300

    form-cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GoogleAnalyticsService } from 'ngx-google-analytics';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'poc';
+export class AppComponent implements OnInit {
+  isLinear = true;
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+  acceptTerms: boolean = false;
+  constructor(
+    private _formBuilder: FormBuilder,
+    private gaService: GoogleAnalyticsService
+  ) { }
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      name: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      tel: [null, [Validators.required]],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      cep: ['', Validators.required],
+    });
+  }
+  
+
+  onClick2() {
+
+    this.gaService.event("button_click")
+    console.log('Click1')
+  }
+  sendDataGA() {
+    console.log( this.firstFormGroup.value)
+    this.gaService.gtag('event', "formulario_cadastro", this.firstFormGroup.value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    NgxGoogleAnalyticsModule.forRoot('G-WNR5S8CYDH'),
+    NgxGoogleAnalyticsRouterModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
